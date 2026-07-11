18d6b39521ffc17a49e88e382bdff3fae8060e7d
Velomatras <gln1981@yandex.ru>
2026-07-11 13:29:43 +0300

    тест

--- views/parts/css.blade.php
+++ views/parts/css.blade.php
@@ -2,3 +2,6 @@
 
 <link href="{{$_SERVER['REQUEST_SCHEME']}}://{{$_SERVER['SERVER_NAME']}}/assets/templates/veloway/css/style7.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="/manager/media/style/common/bootstrap/css/bootstrap.min.css">
+
+<!-- стили -->
+
